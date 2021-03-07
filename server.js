8d25b2550d4b9c98efabd1511e7d1b9ebeab16ab
Arsh Kaushik <66227465+ArshKaushik@users.noreply.github.com>
2021-03-08 00:23:16 +0530

    add cart & auth

--- server.js
+++ server.js
@@ -9,12 +9,24 @@ const PORT = process.env.PORT || 80
 app.use(express.static(path.join(__dirname, 'public')))
 
 // Set template engine
-// app.use(expressLayout)
+app.use(expressLayout)
 app.set('views', path.join(__dirname, '/resources/views'))
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
     res.render('home')
+})
+
+app.get('/cart', (req, res) => {
+    res.render('customers/cart')
+})
+
+app.get('/login', (req, res) => {
+    res.render('auth/login')
+})
+
+app.get('/register', (req, res) => {
+    res.render('auth/register')
 })
 
 app.listen(PORT, (req, res) => {
