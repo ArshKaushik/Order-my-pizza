86006ded22cc9c28767c7801b742f30ae83282af
Arsh Kaushik <66227465+ArshKaushik@users.noreply.github.com>
2021-03-26 02:21:33 +0530

    auth system

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const mongoose = require('mongoose')
 const session = require('express-session')
 const flash = require('express-flash')
 const MongoDbStore = require('connect-mongo')(session)
+const passport = require('passport')
 const PORT = process.env.PORT || 80
 
 // Database
@@ -38,13 +39,21 @@ app.use(session({
 
 app.use(flash())
 
+// Passport config
+const passportInit = require('./app/config/passport')
+passportInit(passport)
+app.use(passport.initialize())
+app.use(passport.session())
+
 // Asset
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({extended: false}))
 app.use(express.json())
 
 // Global Middlewares
 app.use((req, res, next) => {
     res.locals.session = req.session
+    res.locals.user = req.user
     next()
 })
 
